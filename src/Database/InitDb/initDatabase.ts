fbdef259aa5eb338cee123785c3f35d26726871e
unknown <mickael.croquet@laposte.net>
2019-04-12 23:53:43 +0200

    Adding more refacto

--- src/Database/InitDb/initDatabase.ts
+++ src/Database/InitDb/initDatabase.ts
@@ -3,12 +3,12 @@ import { InitQuery } from "./InitQuery";
 import { dbPath } from "../dbUtils";
 import { navigate } from "@reach/router";
 import { Database } from "sqlite3";
-
+import {table} from '../dbUtils'
 const initDatabase = () => {
   const db = new Database(dbPath);
   db.get(
     select()
-      .from("User")
+      .from(table.user)
       .toString(),
     (err, row) => {
       if (err) {
--- src/Page/Article/Article.container.tsx
+++ src/Page/Article/Article.container.tsx
@@ -1,15 +1,19 @@
-import React, { useState, useEffect, FunctionComponentElement, Component } from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
 import { ArticleComponent } from "./Article.component";
 import { ArticleService } from "./Article.service";
 import { Article } from "../../Common/Interface/Article.interface";
+interface routerProps {
+  articleId: number;
+}
 
-const ArticleContainer = (props: any) => {
+type ArticleContainerProps = FunctionComponent<{ articleId: number } | any>;
+const ArticleContainer: ArticleContainerProps = props => {
   const [article, setArticle] = useState<Article>({
     contenu: "",
     id: 0,
     image_entete: { size: 0, type: "", slice: {} },
     statut: "",
-    titre: "",
+    titre: ""
   });
   const ArticleProps = {
     article: article

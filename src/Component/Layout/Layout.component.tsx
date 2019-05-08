ba834b1051edca3d90ddb81d5761672aa0459358
Croquet Mickael <mickael.croquet@laposte.net>
2019-05-08 15:49:03 +0200

    Creating modal component

--- src/Component/Layout/Layout.component.tsx
+++ src/Component/Layout/Layout.component.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { Sidebar } from "./Sidebar";
+import { AccountSettings } from "./Sidebar/AccountSettings";
 import "./layout.css";
 interface Props {
   children: ReactNode;
@@ -8,6 +9,7 @@ const Layout = (props: Props) => (
   <div className="h-screen">
     <div className="flex h-screen">
       <Sidebar />
+      <AccountSettings />
       <div className="w-full flex flex-wrap mb-8">{props.children}</div>
     </div>
   </div>
